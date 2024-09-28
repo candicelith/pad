ec45ff9030da3ebe68a39b2061e199b8b21eb8cd
candicelith <irenetalithatyasraharjo@mail.ugm.ac.id>
2024-09-28 14:21:28 +0700

    first commit

---
+++ resources/views/content/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    @extends('layout.headerFooter')
+
+    @section('content')
+        <div class="max-w-screen-xl mx-auto p-4">
+            <h1 class="text-3xl font-semibold text-gray-900 dark:text-white">Home</h1>
+            <p class="mt-4 text-gray-600 dark:text-gray-400">Welcome to the home page.</p>
+        </div>
+    @endsection
+
+</body>
+</html>
